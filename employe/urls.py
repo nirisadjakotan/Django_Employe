1f1a36e0e4f0cd8b7e23aa42e84a174f63c1d588
Niris Adjakotan <nirisadk@gmail.com>
2026-01-29 09:32:35 +0100

    First Commit

---
+++ employe/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.liste_employes, name="liste_employes"),
+    path('ajouter/', views.ajouter_employe, name="ajouter_employe"),
+    path('modifier/<int:id>', views.modifier_employe, name="modifier_employe"),
+    path('supprimer/<int:id>', views.supprimer_employe, name="supprimer_employe"),
+]
